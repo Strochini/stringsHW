4b50890f6465edf65ed4dc04a15b85bafceb829f
Strochini <0.618@bk.ru>
2022-11-21 19:31:54 +0300

    HW Done

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
-class Main {
+public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        // Task 1, 2, 3
+
+        String firstName = "Семён";
+        String middleName = "Семёнович";
+        String lastName = "Иванов";
+        String fullName = (lastName + " " + firstName + " " + middleName);
+        String fullNameE = fullName.replace("ё", "е");
+        System.out.println("\"ФИО сотрудника - " + fullNameE + "\"\n");
+
+        String upperFullName = fullNameE.toUpperCase();
+        System.out.println("\"Данные ФИО сотрудника для заполнения отчета - " + upperFullName + "\"\n");
     }
 }
